cf99a9b98a7fe31c0cf17154c56c5d63d38726b7
giovanny.agama6161 <giovanny.agama6161@utc.edu.ec>
2025-07-13 22:39:40 -0500

    mejorando index

--- resources/views/riesgos/index.blade.php
+++ resources/views/riesgos/index.blade.php
@@ -2,7 +2,50 @@
 @section('contenido')
 
 <h1>LISTADO DE ZONAS DE RIESGO </h1>
-<a href="{{}}">Crear Nueva Zona de Riesgo</a>
+<a href="{{ route('riesgos.create') }}">Crear Nueva Zona de Riesgo</a>
+&nbsp;&nbsp;&nbsp;&nbsp;
 
+<table class="table table-striped table-bordered" style="width:80%; margin:auto;">
+    <thead class="table-dark">
 
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Nivel de riesgo</th>
+            <th>Documento</th>
+            <th>latitud</th>
+            <th>longitud</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($riesgos as $riesgo)
+        <tr>
+            <td>{{ $riesgo->id }}</td>
+            <td>{{ $riesgo->nombre }}</td>
+            <td>{{ $riesgo->descripcion }}</td>
+            <td>{{ $riesgo->nivel_riesgo }}</td>
+            <td>
+                @if($riesgo->documento)
+                    <a href="{{ asset('storage/' . $riesgo->documento) }}" target="_blank">Ver Documento</a>
+                @else
+                    No disponible
+                @endif
+            </td>
+            <td>{{ $riesgo->latitud }}</td>             
+            <td>{{ $riesgo->longitud }}</td>
+            <td>
+                <a href="{{ route('riesgos.edit', $riesgo->id) }}" class="btn btn-primary">Editar</a>
+                <form action="{{ route('riesgos.destroy', $riesgo->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+    
 @endsection
