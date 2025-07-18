d90ad70da49814256c7fe2fcbc2c1264753b44b4
giovanny.agama6161 <giovanny.agama6161@utc.edu.ec>
2025-07-17 21:33:45 -0500

    creando vista

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Riesgo;
+use App\Models\Segura;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class ReporteController extends Controller
 {
-    //
+    public function reporteZonas()
+    {
+        
+        $riesgos = Riesgo::all();
+        $seguras = Segura::all();
+
+        
+        $pdf = Pdf::loadView('reportes.zonas_pdf', compact('riesgos', 'seguras'))
+                  ->setPaper('a4', 'landscape'); // Formato horizontal
+
+        // Descargar el archivo
+        return $pdf->download('reporte_zonas.pdf');
+    }
 }
